fb949e5ef74af6563b5b35677fc1ad4fb9ab9fb4
Sandesh-Pyakurel <pyakurelsandesh14@gmail.com>
2024-06-12 00:24:11 +0545

    Initial commit

---
+++ auction/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .models import CustomUser
+
+
+class UserSignupForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password1', 'password2']
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
---
+++ auction/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.template import loader
+
+from .forms import UserSignupForm, UserLoginForm
+
+
+def home_view(request):
+    template = loader.get_template('home.html')
+
+    return HttpResponse(template.render(request=request))
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserSignupForm()
+    template = loader.get_template('signup_page.html')
+
+    return HttpResponse(template.render({'form': form}, request=request))
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+
+                return redirect('home')
+    else:
+        form = UserLoginForm()
+    template = loader.get_template('login_page.html')
+    return HttpResponse(template.render({'form': form}, request=request))
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
