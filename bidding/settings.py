97bc07184772ca2ed1bcd0e4449402e3958eaefe
SusheelThapa <susheelthapa90@gmail.com>
2024-06-14 20:32:01 +0545

    feat: login page

--- bidding/settings.py
+++ bidding/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 from shutil import which
 
-NPM_BIN_PATH = which("npm")
+# NPM_BIN_PATH = which("npm")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
